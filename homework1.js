c90f24f98df1148414b662fc0a957bd26ac9aa31
Vasyl_Hirnyi <vasylgirnyi@email.com>
2025-06-07 14:56:38 +0300

    Додано файл homework1.js до гілки js_homework1

---
+++ homework1.js
@@ -0,0 +1,56 @@
+// 'number' + 3 + 3
+console.log('number' + 3 + 3); // 'number33'
+
+// null + 3
+console.log(null + 3); // 3
+
+// 5 && "qwerty"
+console.log(5 && "qwerty"); // "qwerty"
+
+// +'40' + +'2' + "hillel"
+console.log(+'40' + +'2' + "hillel"); // "42hillel"
+
+// '10' - 5 === 6
+console.log('10' - 5 === 6); // false
+
+// true + false
+console.log(true + false); // 1
+
+// '4px' - 3
+console.log('4px' - 3); // NaN
+
+// '4' - 3
+console.log('4' - 3); // 1
+
+// '6' + 3 ** 0
+console.log('6' + 3 ** 0); // "61"
+
+// 12 / '6'
+console.log(12 / '6'); // 2
+
+// '10' + (5 === 6)
+console.log('10' + (5 === 6)); // "10false"
+
+// null == ''
+console.log(null == ''); // false
+
+// 3 ** (9 / 3)
+console.log(3 ** (9 / 3)); // 27
+
+// !!'false' == !!'true'
+console.log(!!'false' == !!'true'); // true
+
+// 0 || '0' && 1
+console.log(0 || '0' && 1); // 1
+
+// (+null == false) < 1
+console.log((+null == false) < 1); // true
+
+// false && true || true
+console.log(false && true || true); // true
+
+// false && (false || true)
+console.log(false && (false || true)); // false
+
+// (+null == false) < 1 ** 5
+console.log((+null == false) < 1 ** 5); // true
